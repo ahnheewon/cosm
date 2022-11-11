2d985f3fa6dbf479c48c244be3e68a60ed95a98c
ahnheewon <aovilr@gmail.com>
2022-11-11 17:37:55 +0900

    11/11 DB연결 체크완료.

--- Cosm/src/main/java/com/prj/cosm/CosmApplication.java
+++ Cosm/src/main/java/com/prj/cosm/CosmApplication.java
@@ -1,14 +1,17 @@
 package com.prj.cosm;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@MapperScan(basePackages = "com.prj.cosm.mapper")
 @SpringBootApplication
 public class CosmApplication {
 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CosmApplication.class, args);
 	}
@@ -17,5 +20,6 @@ public class CosmApplication {
 	String home() {
 		return "cosm start";
 	}
+
 
 }
