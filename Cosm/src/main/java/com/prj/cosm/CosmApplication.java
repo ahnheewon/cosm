a6ce34f83cdc89b5c1aff95006560613e74a97a9
wlswj <wlswj@DESKTOP-P87QLDV>
2022-11-13 14:35:11 +0900

    jjw default template

--- Cosm/src/main/java/com/prj/cosm/CosmApplication.java
+++ Cosm/src/main/java/com/prj/cosm/CosmApplication.java
@@ -3,10 +3,9 @@ package com.prj.cosm;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
 @MapperScan(basePackages = "com.prj.cosm.mapper")
 @SpringBootApplication
 public class CosmApplication {
@@ -16,9 +15,9 @@ public class CosmApplication {
 		SpringApplication.run(CosmApplication.class, args);
 	}
 	
-	@RequestMapping("/")
-	String home() {
-		return "cosm start";
+	@GetMapping("/")
+	public String home() {
+		return "index";
 	}
 
 
