0d10aee91509eade17d20c3fb6d6a4ca88d28a78
admin <admin@YD-15>
2022-11-16 15:32:23 +0900

    2022-11-16 15:32 수정안

--- Cosm/src/main/java/com/prj/cosm/user/emp/service/impl/EmpServiceImpl.java
+++ Cosm/src/main/java/com/prj/cosm/user/emp/service/impl/EmpServiceImpl.java
@@ -3,6 +3,7 @@ package com.prj.cosm.user.emp.service.impl;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.prj.cosm.user.emp.mapper.EmpMapper;
@@ -14,30 +15,45 @@ public class EmpServiceImpl implements EmpService {
 
 	@Autowired
 	EmpMapper mapper;
-	
+
 	@Override
 	public List<EmpVO> empSelectList() {
-		return mapper.empSelectList();
+		return mapper.empSelectList(0);
 	}
 
 	@Override
-	public EmpVO empSelect(EmpVO vo) {
-		return mapper.empSelect(vo);
+	public EmpVO empSelect(String usersNo) {
+		return mapper.empSelect(usersNo);
 	}
 
 	@Override
-	public EmpVO empInsert(EmpVO vo) {
+	public int empInsert(EmpVO vo) {
+		BCryptPasswordEncoder enco = new BCryptPasswordEncoder();
+		vo.setUsersPassword(enco.encode(vo.getUsersPassword()));
+		if (vo.getUsersAuthor() == null || vo.getUsersAuthor().equals("")) {
+			vo.setUsersAuthor("고객");
+		}
 		return mapper.empInsert(vo);
 	}
 
 	@Override
-	public EmpVO empUpdate(EmpVO vo) {
+	public int empUpdate(EmpVO vo) {
 		return mapper.empUpdate(vo);
 	}
 
 	@Override
-	public EmpVO empDelete(EmpVO vo) {
+	public int empDelete(EmpVO vo) {
 		return mapper.empDelete(vo);
+	}
+
+	@Override
+	public List<String> getAuthorList() {
+		return mapper.getAuthorList();
+	}
+
+	@Override
+	public int checkId(String id) {
+		return mapper.checkId(id);
 	}
 
 }
