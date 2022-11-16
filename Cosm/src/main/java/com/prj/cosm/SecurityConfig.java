507d01e6a1f41c0148aa965cac24a5f6b3868ba6
hwangyongju <hwangyongju@192.168.0.39>
2022-11-16 06:40:23 -0800

    홈

--- Cosm/src/main/java/com/prj/cosm/SecurityConfig.java
+++ Cosm/src/main/java/com/prj/cosm/SecurityConfig.java
@@ -39,7 +39,7 @@ public class SecurityConfig {
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		http.authorizeHttpRequests((requests) -> requests
-				.antMatchers("/top", "/login", "/joinForm", "/empCheckId", "/empInsert", "/userSelect","/planUpdate").permitAll()
+				.antMatchers("/top", "/login", "/joinForm", "/empCheckId", "/empInsert", "/userSelect","/planUpdate", "/planInsert", "/insUpdate", "/insInsert").permitAll()
 				.antMatchers("/*").hasAuthority("ROLE_D0101").anyRequest().authenticated()).formLogin()
 				.loginPage("/login").usernameParameter("userId").loginProcessingUrl("/login")
 				.successHandler(successHandler()).and().logout().logoutUrl("/logout").logoutSuccessUrl("/login")
