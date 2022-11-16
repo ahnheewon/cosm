9c740418388a653fd8a1a0047ce87d09a2410613
백진희 <lovely+gingin@naver.com>
2022-11-16 15:55:45 +0900

    Gini-11-16

--- Cosm/src/main/java/com/prj/cosm/MainController.java
+++ Cosm/src/main/java/com/prj/cosm/MainController.java
@@ -9,8 +9,11 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.prj.cosm.sales.orders.service.OrdersService;
+import com.prj.cosm.sales.orders.service.OrdersVO;
 import com.prj.cosm.user.emp.service.EmpService;
 import com.prj.cosm.user.emp.service.EmpVO;
 
@@ -20,6 +23,9 @@ public class MainController {
 
 	@Autowired
 	EmpService service;
+
+	@Autowired
+	OrdersService ser;
 
 	// 첫 화면
 	@GetMapping("/main")
@@ -78,4 +84,66 @@ public class MainController {
 	public int empCheckId(String usersId) {
 		return service.checkId(usersId);
 	}
+
+	// 고객 --------------------------------
+	// 첫 화면
+	@RequestMapping("/main")
+	public List<OrdersVO> client(Model model) {
+		return ser.salesOrderList();
+	}
+
+	// 고객 주문목록 관리 메인
+	@RequestMapping("/order")
+	public String cilentorder(Model model) {
+		return "client/order";
+	}
+
+	// 고객 - 주문목록데이터
+	@RequestMapping("/orderList")
+	@ResponseBody
+	public List<OrdersVO> clientorderList(Model model) {
+		model.addAttribute("id", ser.getOrderNo());
+		return ser.salesOrderList();
+	}
+
+	// 고객 주문관리 메인
+	@RequestMapping("/insert")
+	public String clientOrder(Model model) {
+		return "client/insert";
+	}
+
+	// 마이페이지
+	@RequestMapping("/my")
+	public String clientMypage(Model model) {
+		return "client/myPage";
+	}
+
+	// 영업팀 -----------------------------------
+	// 영업 - 주문조회 리스트
+	@ResponseBody
+	@GetMapping("/ajax/orders")
+	public List<OrdersVO> ajaxOrder(Model model) {
+		return ser.salesOrderList();
+	}
+
+	// 메인페이지 - 주문관리
+	@RequestMapping("/test")
+	public String test(Model model) {
+		return "sales/dtest";
+	}
+
+	@GetMapping("/orders")
+	public String salesorder(Model model) {
+		return "sales/orders";
+	}
+
+	// 사원 - 주문목록데이터
+	@GetMapping("/ordersList")
+	@ResponseBody
+	public List<OrdersVO> salesorderList(Model model) {
+		model.addAttribute("id", ser.getOrderNo());
+		return ser.salesOrderList();
+	}
+
+	/* main - 주문목록조회 = ajax, get */
 }
