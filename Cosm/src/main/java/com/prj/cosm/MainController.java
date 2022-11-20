405b72188fbc7b36ef8c288517a9685ff60d9973
wlswj <wlswj@DESKTOP-P87QLDV>
2022-11-20 18:38:12 +0900

    asdf

--- Cosm/src/main/java/com/prj/cosm/MainController.java
+++ Cosm/src/main/java/com/prj/cosm/MainController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
 import com.prj.cosm.equipment.equip.service.EquipService;
 import com.prj.cosm.equipment.equip.service.EquipVO;
 import com.prj.cosm.material.material.service.MaterialService;
@@ -26,18 +27,15 @@ import com.prj.cosm.produce.plan.service.PlanVO;
 import com.prj.cosm.produce.regist.service.RegistService;
 import com.prj.cosm.produce.regist.service.RegistVO;
 import com.prj.cosm.sales.client.service.ClientService;
-import com.prj.cosm.sales.client.service.ClientVO;
 import com.prj.cosm.sales.orders.service.OrdersService;
 import com.prj.cosm.sales.orders.service.OrdersVO;
 import com.prj.cosm.user.emp.service.EmpService;
-import com.prj.cosm.user.emp.service.EmpVO;
 
 @Controller
 @CrossOrigin("*")
 public class MainController {
 
-	@Autowired
-	EmpService service;
+	
 
 	@Autowired
 	OrdersService oService;
@@ -139,98 +137,7 @@ public class MainController {
 
 	}
 
-//	여기서부터 진정욱
 
-	// 첫 화면
-	@GetMapping("/main")
-	public String main(Model model) {
-		return "/index";
-	}
-
-	@GetMapping("/login")
-	public String login() {
-		return "/login";
-	}
-
-	@GetMapping("/joinForm")
-	public String joinForm() {
-		return "/users/client/joinForm";
-	}
-
-	@GetMapping("/top")
-	public String top() {
-		return "/top";
-	}
-
-	@GetMapping("/userList")
-	public String empList(Model model) {
-		model.addAttribute("authorList", service.getAuthorList());
-		return "/users/emp/empList";
-	}
-
-	@GetMapping("/clientList")
-	public String clientList(Model model) {
-		model.addAttribute("authorList", service.getAuthorList());
-		return "/users/emp/empList";
-	}
-
-	@GetMapping("/getUserList")
-	@ResponseBody
-	public List<EmpVO> getUserList() {
-		return service.empSelectList();
-	}
-
-	@PostMapping("/userSelect")
-	@ResponseBody
-	public EmpVO userSelect(String usersNo) {
-		return service.userInfoSelect(usersNo);
-	}
-
-	@PostMapping("userInsert")
-	public String empInsert(EmpVO empVO) {
-		service.userInsert(empVO);
-		return "redirect:userList";
-	}
-
-	@PostMapping("clientInsert")
-	public String clientInsert(EmpVO empVO, ClientVO cVO) {
-		service.userInsert(empVO);
-		return "redirect:userList";
-	}
-
-	@PostMapping("userUpdate")
-	public String empUpdate(EmpVO empVO) {
-		service.userUpdate(empVO);
-		return "redirect:userList";
-	}
-
-	@PostMapping("userDelete")
-	@ResponseBody
-	public int userDelete(EmpVO empVO) {
-		return service.userDelete(empVO);
-	}
-
-	@GetMapping("/userInfo")
-	public String userInfo() {
-		return "/users/userInfo";
-	}
-
-//	@RequestMapping(value = "/checkId", method = RequestMethod.POST)
-	@GetMapping("/userCheckId")
-	@ResponseBody
-	public int userCheckId(String usersId) {
-		return service.checkId(usersId);
-	}
-
-	@PostMapping("/userConfirm")
-	@ResponseBody
-	public int userConfirm(@RequestBody EmpVO vo) {
-		System.out.println("--------------------------------");
-		System.out.println(vo);
-		return service.userConfirm(vo);
-	}
-
-//	여기까지 진정욱
 
 	// 영업 start =======================================================
 	// 고객 주문목록 페이지
