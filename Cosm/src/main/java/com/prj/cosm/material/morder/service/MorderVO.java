1a9771090344fec85eee8ef16f9c27645fed8ccc
cococ <cococ@LAPTOP-LM7QD4N7>
2022-11-20 13:25:35 +0900

    ~221119 작업

---
+++ Cosm/src/main/java/com/prj/cosm/material/morder/service/MorderVO.java
@@ -0,0 +1,47 @@
+package com.prj.cosm.material.morder.service;
+
+import java.util.Date;
+
+public class MorderVO {
+	
+	// 1. 자재 입출고 - 입,출고된 자재 리스트
+	
+	private String mioLotNo;// 입고LOT 
+	private String mioNo;// 입고번호
+	
+	private Date mioInDate;// 입고일자
+	private Date mioOutDate;// 출고일자
+	
+	private Integer mComId;// 거래처번호
+	private String mComNm;// 거래처이름
+	private String mcomMrgNm;// 담당자이름
+	
+	private int mioInQntt; // 입고수량
+	private int mioOutQntt; // 출고수량
+	
+	
+	
+	// 진행코드
+	
+	
+	// 2. 발주관리
+	
+	// 자재번호
+	
+	// 발주명
+	
+	// 총금액
+	
+	// 신청일자
+	
+	// 발주단가
+	
+	// 발주수량
+	
+	//진행코드
+	
+	
+	
+	
+
+}
