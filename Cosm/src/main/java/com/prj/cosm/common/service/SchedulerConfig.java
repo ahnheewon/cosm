8982f076765b1cfd14edd16c294587002f95791e
admin <admin@YD-15>
2022-12-02 09:25:44 +0900

    sc

--- Cosm/src/main/java/com/prj/cosm/common/service/SchedulerConfig.java
+++ Cosm/src/main/java/com/prj/cosm/common/service/SchedulerConfig.java
@@ -1,9 +1,11 @@
 package com.prj.cosm.common.service;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.SchedulingConfigurer;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.scheduling.config.ScheduledTaskRegistrar;
 
+@Configuration
 public class SchedulerConfig implements SchedulingConfigurer {
 	private final static int POOL_SIZE = 10;
 
