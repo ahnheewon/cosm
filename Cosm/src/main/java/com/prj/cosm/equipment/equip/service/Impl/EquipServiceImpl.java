c5e21b2bde13fe6e64b200efbeadbf8a6a4aaef4
admin <admin@YD-15>
2022-12-05 15:50:53 +0900

    asd

--- Cosm/src/main/java/com/prj/cosm/equipment/equip/service/Impl/EquipServiceImpl.java
+++ Cosm/src/main/java/com/prj/cosm/equipment/equip/service/Impl/EquipServiceImpl.java
@@ -259,12 +259,12 @@ public class EquipServiceImpl implements EquipService {
 //		}
 //		insMapper.updateInsPlay(insVO);
 //	}
-	@Scheduled(fixedDelay = 1000)
+//	@Scheduled(fixedDelay = 1000)
 	public void updateState() {
 		mapper.setProState();
 	}
 
-	@Scheduled(fixedDelay = 1000)
+//	@Scheduled(fixedDelay = 1000)
 	public void doWork() {
 		InsVO ivo = insMapper.getRecentQntt();
 		EquipVO evo = mapper.getRTPState();
